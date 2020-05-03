a9747ced2b52bc128f4595db8bd1ba9eecd3071d
rlmahesh777 <31091307+rlmahesh777@users.noreply.github.com>
2020-05-03 13:23:04 +0530

    Create csv.py

---
+++ Files/csv.py
@@ -0,0 +1,24 @@
+file = open('csv_data.txt', 'r')
+lines = file.readlines()
+file.close()
+
+lines = [line.strip() for line in lines[1:]]
+
+for line in lines:
+    person_data = line.split(',')
+    name = person_data[0].title()
+    age = person_data[1]
+    university = person_data[2].title()
+    degree = person_data[3].capitalize()
+
+    print(f'{name} is {age}, studying {degree} at {university}.')
+    
+    
+    
+    
+    # csv_data.txt
+name,age,degree,university
+mahesh,23,bsc,svu
+prasu,22,bcom,klm
+jagga,29,physics,vit
+sathi,35,msc,kmm
